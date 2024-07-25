4692ebc824d7421afae111aed612ad0f7be43447
arekzas77 <arekzaskalski@interia.pl>
2024-07-25 12:48:27 +0200

    start map project: html css js

---
+++ js/mojkod.js
@@ -0,0 +1,34 @@
+//map initialization
+const map = L.map('map',{
+	zoomControl: true,
+	maxZoom:19
+				});
+
+map.setView([50.2, 19.93],8);		
+map.setMaxBounds(map.getBounds());
+
+const openStreet=L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 20,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+const beztla = L.tileLayer('',{maxZoom: 20});	
+
+const TMCEicon = L.icon({
+	iconUrl: 'css/images/logoTMCE1.jpg',
+	iconSize: [100, 50],
+	iconAnchor: [22, 94],
+	popupAnchor: [0, -94],
+   	});
+		
+const TMCEmarker = L.marker([50.02658, 19.929859], {
+		icon: TMCEicon}).addTo(map).bindPopup("<h3>Już za niedługo to będzie najlepszy portal mapowy na świecie;)</h3> <center><img src='./css/images/champions2.jpg'/><center>")
+	
+const baseLayers = {
+		"OpenStreet": openStreet,
+		"Brak": beztla};
+const overlayMaps= {
+	"Pinezka TMCE": TMCEmarker,
+	};
+	
+const layerControl = L.control.layers(baseLayers, overlayMaps, {collapsed : false}).addTo(map);
